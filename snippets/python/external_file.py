388a459db1b169600aa021d51fd448c3275ab37e
Coppelia <mfreese@gmx.ch>
2022-01-12 08:19:25 +0100

    detail

--- snippets/python/external_file.py
+++ snippets/python/external_file.py
@@ -1,8 +1,8 @@
 #@name External file inclusion
-#python
 #luaExec: pythonFile=<path/to/pythonfile.py>
 
 # use above luaExec if you wish to execute code contained in an external file
+# Should be on the second line, directly after the language tag <#python>
 # If 'pythonFile' is not an absolute path, the file is searched in:
 # <CoppeliaSim executable path>/ or
 # <CoppeliaSim executable path>/python or
