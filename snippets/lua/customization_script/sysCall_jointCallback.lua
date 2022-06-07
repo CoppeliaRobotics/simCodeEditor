8ca292567cfed6bae2b36d060828a9c82c5de4d9
Coppelia <mfreese@gmx.ch>
2022-06-07 10:22:54 +0200

    more args to joint callback

--- snippets/lua/customization_script/sysCall_jointCallback.lua
+++ snippets/lua/customization_script/sysCall_jointCallback.lua
@@ -1,29 +1,32 @@
 function sysCall_jointCallback(inData)
-    -- This function gets called often, so it might slow down the simulation
-    --     (this is called at each dynamic simulation step, by default 10x more often than a child script)
-    -- We have:
-    -- inData.first : whether this is the first call from the physics engine, since the joint
-    --                was initialized (or re-initialized) in it.
+    -- inData.mode : sim.jointmode_kinematic or sim.jointmode_dynamic
+    --
+    -- inData.handle : the handle of the joint associated with this script
     -- inData.revolute : whether the joint associated with this script is revolute or prismatic
     -- inData.cyclic : whether the joint associated with this script is cyclic or not
-    -- inData.handle : the handle of the joint associated with this script
     -- inData.lowLimit : the lower limit of the joint associated with this script (if the joint is not cyclic)
     -- inData.highLimit : the higher limit of the joint associated with this script (if the joint is not cyclic)
-    -- inData.passCnt : the current dynamics calculation pass. 1-10 by default. See next item for details.
+    -- inData.currentPos : the current position
+    -- inData.targetPos : the desired position (if joint is dynamic, or when sim.setJointTargetPosition was called)
+    -- inData.targetVel : the desired velocity (if joint is dynamic, or when sim.setJointTargetVelocity was called)
+    -- inData.initVel : the desired initial velocity (if joint is kinematic and when sim.setJointTargetVelocity
+    --                  was called with a 4th argument)
+    -- inData.errorValue : targetPos-currentPos (with revolute cyclic joints, the shortest cyclic distance)
+    -- inData.maxVel : a maximum velocity, taken from sim.setJointTargetPosition or 
+    --                 sim.setJointTargetVelocity's 3rd argument)
+    -- inData.maxAccel : a maximum acceleration, taken from sim.setJointTargetPosition or
+    --                   sim.setJointTargetVelocity's 3rd argument)
+    -- inData.maxJerk : a maximum jerk, taken from sim.setJointTargetPosition or
+    --                  sim.setJointTargetVelocity's 3rd argument)
+    -- inData.first : whether this is the first call from the physics engine, since the joint
+    --                was initialized (or re-initialized) in it.
+    -- inData.passCnt : the current dynamics calculation pass. 1-10 by default
     -- inData.totalPasses : the number of dynamics calculation passes for each "regular" simulation pass.
-    --                      10 by default (i.e. 10*5ms=50ms which is the default simulation time step)
-    -- inData.currentPos : the current position of the joint
-    -- inData.targetPos : the desired position of the joint
-    -- inData.errorValue : targetPos-currentPos (with revolute cyclic joints we take the shortest cyclic distance)
     -- inData.effort : the last force or torque that acted on this joint along/around its axis. With Bullet,
     --                 torques from joint limits are not taken into account
     -- inData.dynStepSize : the step size used for the dynamics calculations (by default 5ms)
-    -- inData.targetVel : the joint target velocity (as set in the user interface)
-    -- inData.maxForce : the joint maximum force/torque (as set in the user interface)
-    -- inData.velUpperLimit : the joint velocity upper limit (as set in the user interface)
-    --
-    -- Make sure that the joint is dynamically enabled, is in force/torque mode, motor enabled and
-    -- control loop enabled, otherwise this function won't be called
+    -- inData.force : the joint force/torque, as set via sim.setJointTargetForce
+    -- inData.velUpperLimit : the joint velocity upper limit
 
     if inData.first then
         PID_P=0.1
@@ -52,10 +55,10 @@ function sysCall_jointCallback(inData)
     
     -- 4. Calculate the velocity needed to reach the position in one dynamic time step:
     local maxVelocity=ctrl/inData.dynStepSize -- max. velocity allowed.
-    if (maxVelocity>inData.velUpperLimit) then
+    if (maxVelocity&gt;inData.velUpperLimit) then
         maxVelocity=inData.velUpperLimit
     end
-    if (maxVelocity<-inData.velUpperLimit) then
+    if (maxVelocity&lt;-inData.velUpperLimit) then
         maxVelocity=-inData.velUpperLimit
     end
     local forceOrTorqueToApply=inData.maxForce -- the maximum force/torque that the joint will be able to exert
@@ -66,4 +69,11 @@ function sysCall_jointCallback(inData)
     outData.velocity=maxVelocity
     outData.force=forceOrTorqueToApply
     return outData
+    
+    -- Expected return data:
+    -- For kinematic joints:
+    -- outData={position=pos, velocity=vel, immobile=false}
+    -- 
+    -- For dynamic joints:
+    -- outData={force=f, velocity=vel}
 end
